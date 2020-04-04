5789a6648168073813cbbf7f2a8e6f7b1f4ac2fe
Gulshan <chandoliagulahan@gmail.com>
2020-04-04 16:05:07 +0530

    this is my first commit

---
+++ Guess.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 11:33:19 2020
+
+@author: Gulshan Chandolia
+"""
+
+import random
+
+num = random.randint(1,20)
+
+chances = 0
+
+while chances < 5:
+    
+    guess = int(input("Guess a Number between 1 to 20:"))
+    
+    if guess == num :
+        
+        print("Correct Guess")
+        
+        break
+    
+    elif guess < num:
+        
+        print("Guess is too low Guess higher number")
+        
+    else:
+        
+        print("Guess is too high Guess lower number")
+        
+    chances += 1
+    
+if not chances < 5:
+    
+    print("Wrong Guesses : The Number is ")
+    
+    print(num)
+    
+input("Enter any key to exit")
+
